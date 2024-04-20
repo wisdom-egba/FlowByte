0b5ec4cdedf54127db99a585bd577754ae8ee128
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2024-04-20 23:41:53 +0100

    fixed nav-bar bug

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-const Button = ({ bg = "bg-black", content }) => {
+const Button = ({
+  bg = "bg-[#6455C2]",
+  content,
+  hover = "hover:bg-[#C713B5]",
+}) => {
   return (
-    <button className={`${bg} text-white px-3 py-2 rounded-md`}>
+    <button className={`${bg} ${hover} text-white px-3 py-2 rounded-md`}>
       {content}
     </button>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
   };
   return (
     <>
-      <div className="flex justify-between px-4 pt-3 w-full">
+      <div className="flex justify-between px-4 pt-3 w-full border-b">
         <div className="flex items-center gap-2 z-10">
           <img src={logo} alt="table-24" className=" w-[35px] md:w-[40px]" />
           <h1 className=" text-[24px] md:text-[34px] text-[#3d3d3d] font-black">
@@ -23,8 +23,8 @@ const Header = () => {
           <div
             className={`${
               toggle
-                ? "hidden"
-                : " bg-slate-200 fixed top-0 right-0 w-2/3 md:w-full h-full flex-col flex items-start outline-none z-20 py-10 md:py-0  px-3 md:px-0 "
+                ? " bg-slate-200 fixed top-0 right-0 w-2/3 md:w-full h-full flex-col flex items-start outline-none z-20 py-10 md:py-0  px-3 md:px-0  "
+                : "hidden"
             } md:outline-none md:flex md:flex-row gap-6 md:gap-7 md:items-center md:relative md:bg-transparent `}
           >
             <div className="fixed top-0 right-0 py-3 px-2">
